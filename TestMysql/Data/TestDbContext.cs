a97a66118bef916eeedc293c95e36e0a3b10e4e3
fuzzthegreat <fuzz@framedrop.io>
2022-03-30 14:03:18 -0700

    Add project files.

---
+++ TestMysql/Data/TestDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMysql.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
