47cde916901d4b77e41b8ad5e27f9bdc1ee63e1c
fuzzthegreat <fuzz@framedrop.io>
2022-03-30 14:16:05 -0700

    first mig

--- TestMysql/Data/TestDbContext.cs
+++ TestMysql/Data/TestDbContext.cs
@@ -10,5 +10,13 @@ namespace TestMysql.Data
     public class TestDbContext : DbContext
     {
         public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "server=10.0.55.176;user=root;password=t81vDl9RjD;database=testing";
+
+            var version = new MySqlServerVersion(new Version(8, 0));
+            optionsBuilder.UseMySql(connectionString, version);
+        }
     }
 }
--- TestMysql/Program.cs
+++ TestMysql/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestMysql.Data;
+
 namespace TestMysql
 {
     public class TestMysql
@@ -6,6 +8,18 @@ namespace TestMysql
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var context = new TestDbContext();
+
+            var emp = new Employee() { Name = "fuzz 234243", Email = "hiiii"};
+            context.Employees.Add(emp); 
+            context.SaveChanges();
+
+            var list = context.Employees.ToList();
+            foreach(var outputEmp in list)
+            {
+                Console.WriteLine("Employee {0}", emp.Name);
+            }
         }
     }
 }
